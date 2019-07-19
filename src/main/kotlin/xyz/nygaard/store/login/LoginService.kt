6196dd8bf5a280d2db860b8d0db7b104e7da9d32
Eivind Siqveland Larsen <eivind@siqve.land>
2019-07-19 20:52:44 +0200

    base64: omit padding

--- src/main/kotlin/xyz/nygaard/store/login/LoginService.kt
+++ src/main/kotlin/xyz/nygaard/store/login/LoginService.kt
@@ -10,7 +10,7 @@ class LoginService {
         val array = ByteArray(32)
         secureRandom.nextBytes(array)
 
-        return String(Base64.getUrlEncoder().encode(array))
+        return String(Base64.getUrlEncoder().withoutPadding().encode(array))
     }
 
     fun login(key: String) {
