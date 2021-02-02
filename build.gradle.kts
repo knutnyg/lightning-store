ed55d95f081a548c601d9d0f300134a7ba456c6f
Knut Nygaard <knut.nygaard@nav.no>
2021-02-02 17:13:56 +0100

    Bumping all the things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,19 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktorVersion = "1.2.0"
+val ktorVersion = "1.5.1"
 val logbackVersion = "1.2.3"
 val logstashEncoderVersion = "5.1"
 val kotlinVersion = "1.3.40"
 val jacksonVersion = "2.9.7"
 val postgresVersion = "42.2.5"
-val h2Version = "1.4.197"
 val flywayVersion = "5.2.4"
 val hikariVersion = "3.3.0"
+val junitJupiterVersion = "5.6.2"
 
 group = "xyz.nygaard"
 version = "1.1"
 
 plugins {
-    java
-    kotlin("jvm") version "1.3.40"
-    id("com.diffplug.gradle.spotless") version "3.14.0"
-    id("com.github.johnrengelman.shadow") version "4.0.4"
+    kotlin("jvm") version "1.4.0"
 }
 
 buildscript {
@@ -29,13 +23,11 @@ buildscript {
 }
 
 repositories {
-    mavenCentral()
     jcenter()
-    maven(url = "https://dl.bintray.com/kotlin/ktor")
 }
 
 dependencies {
-    compile("org.lightningj:lightningj:0.7.0-Beta")
+    compile("org.lightningj:lightningj:0.12.0-Beta")
     compile("javax.xml.bind:jaxb-api:2.3.1")
     runtime("javax.json:javax.json-api:1.1.2")
 
@@ -55,58 +47,55 @@ dependencies {
 
     //Database
     implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.h2database:h2:$h2Version")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
 
     //Test
-    testImplementation("org.amshove.kluent:kluent:1.49")
-    testImplementation("io.mockk:mockk:1.9")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("io.mockk:mockk:1.10.5")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty") // conflicts with WireMock
+        exclude(group = "junit")
     }
-    testImplementation("com.opentable.components:otj-pg-embedded:0.13.1")
-
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.2") {
-        exclude(group = "org.jetbrains.kotlin")
-    }
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.2") {
-        exclude(group = "org.jetbrains.kotlin")
-    }
-
-
+    testImplementation("com.opentable.components:otj-pg-embedded:0.13.3")
 }
 
 tasks {
-    withType<Jar> {
-        manifest.attributes["Main-Class"] = "xyz.nygaard.BootstrapKt"
+    compileKotlin {
+        kotlinOptions.jvmTarget = "14"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "14"
     }
 
-    create("printVersion") {
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = "xyz.nygaard.BootstrapKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
 
         doLast {
-            println(project.version)
-        }
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    withType<ShadowJar> {
-        transform(ServiceFileTransformer::class.java) {
-            setPath("META-INF/cxf")
-            include("bus-extensions.txt")
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
 
     withType<Test> {
-        useJUnitPlatform {
-            includeEngines("spek2")
-        }
+        useJUnitPlatform()
         testLogging {
-            showStandardStreams = true
+            events("passed", "skipped", "failed")
         }
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "6.8.1"
     }
 }
