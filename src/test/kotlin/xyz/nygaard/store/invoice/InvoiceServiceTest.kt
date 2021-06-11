375a208d56eb7d0abbd8483af0da0bbc548569f8
Knut Nygaard <knutnyg@gmail.com>
2021-06-11 10:31:12 +0200

    Test fetching missing invoice

--- src/test/kotlin/xyz/nygaard/store/invoice/InvoiceServiceTest.kt
+++ src/test/kotlin/xyz/nygaard/store/invoice/InvoiceServiceTest.kt
@@ -1,12 +1,34 @@
 package xyz.nygaard.store.invoice
 
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import xyz.nygaard.TestDatabase
+import xyz.nygaard.lnd.LndClientMock
+import java.util.*
+import kotlin.test.assertNull
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class InvoiceServiceTest {
 
-    @Test
-    fun test() {
+    val lndClientMock = LndClientMock()
+    val embeddedPostgres = EmbeddedPostgres.builder()
+        .setPort(5533).start()
 
+    val invoiceService = InvoiceService(
+        TestDatabase(embeddedPostgres.postgresDatabase),
+        lndClientMock
+    )
+
+    @BeforeAll
+    fun test() {
+        Flyway.configure().dataSource(embeddedPostgres.postgresDatabase).load().migrate()
+    }
+
+    @Test
+    fun `unknown uuid`() {
+        assertNull(invoiceService.getInvoice(UUID.randomUUID()))
     }
 }
