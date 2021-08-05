9f2a649f1b19bbc5f8f6e83e2e659930a6ad2126
Knut Nygaard <knut.nygaard@nav.no>
2021-08-05 17:26:33 +0200

    Kaizen

--- src/test/kotlin/xyz/nygaard/LocalBootstrap.kt
+++ src/test/kotlin/xyz/nygaard/LocalBootstrap.kt
@@ -1,10 +1,5 @@
 package xyz.nygaard
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
 import io.ktor.application.*
 import io.ktor.features.*
@@ -21,13 +16,6 @@ import java.sql.Connection
 import java.util.*
 import javax.sql.DataSource
 
-
-val objectMapper: ObjectMapper = ObjectMapper().apply {
-    registerKotlinModule()
-    registerModule(JavaTimeModule())
-    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-    configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-}
 
 fun main() {
     embeddedServer(Netty, 8081) {
