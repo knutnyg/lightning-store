e3d729f4a9fe4128abeeeeacb17baf6ac094f5e6
Knut Nygaard <knut.nygaard@nav.no>
2021-08-11 16:09:57 +0200

    Kaizen

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -19,21 +19,6 @@ function App() {
             .then(_user => setUser(_user))
     }, 3000)
 
-    const about = () => {
-        return <div>
-            <p>This site is a project for me to learn my way around programming for the lightning network and all
-                the possibilities it brings to web 3.0. I will attempt to explore concepts and techniques that
-                micropayments bring to the table.</p>
-            <p>The lightning network is a global decentralized payment network leveraging the bitcoin block chain.
-                Trading some of the features (like offline wallets) for benefits like massively scalable and near
-                free instant transactions. This could pave the way for a brave new web 3.0 where micropayments
-                replace advertisements and sale of your personal data as the main way to fund a service or a web
-                site.</p>
-            <p>To navigate this site, you need your own lightning wallet. There are many alternatives, but as a
-                quick-start I suggest the custodian wallet BlueWallet.</p>
-        </div>
-    }
-
     return (
         <div className="main">
             <header>
@@ -46,20 +31,34 @@ function App() {
                     <Switch>
                         <Route path="/lsat"><LSATView/></Route>
                         <Route path="/blog"><h1>Blog</h1></Route>
+                        <Route path="/bitcoin-network"><h1>Bitcoin Network</h1></Route>
+                        <Route path="/lightning-network"><h1>Lightning Network</h1>
+                            <p>The lightning network is a global
+                                decentralized payment network leveraging the bitcoin block chain.
+                                Trading some of the features (like offline wallets) for benefits like massively scalable
+                                and near free instant transactions. This could pave the way for a brave new web 3.0
+                                where micropayments replace advertisements and sale of your personal data as the main
+                                way to fund a service or a web site.</p>
+                        </Route>
                         {/*<Route path="/about">{about()}</Route>*/}
                         {/*<Route path="/about">{about()}</Route>*/}
-                        <Route path="/">{about()}</Route>
+                        <Route path="/">
+                            <p>This site is a project for me to learn my way around programming for the lightning
+                                network and all
+                                the possibilities it brings to web 3.0. I will attempt to explore concepts and
+                                techniques that
+                                micropayments bring to the table.</p>
+                        </Route>
                     </Switch>
                 </Router>
-                <footer>
-                    <span>I would love suggestions to what more I could add to my store! Take a look at the code on <a
-                        href={"https://github.com/knutnyg/lightning-store/"}>github</a>.</span>
-                    <span>Connect to my lightning node: 020deb273bd81cd6771ec3397403f2e74a3c22f8f4c052321c30e5c612cf538328@84.214.74.65:9735</span>
-                </footer>
             </div>
+            <footer>
+                    <span>I would love suggestions to what more I could add to my store! Take a look at the code on <a
+                        href={"https://github.com/knutnyg/lightning-store/"}>github</a>. </span>
+                <span>Connect to my lightning node: 020deb273bd81cd6771ec3397403f2e74a3c22f8f4c052321c30e5c612cf538328@84.214.74.65:9735</span>
+            </footer>
         </div>
     );
 }
 
 export default App;
-
