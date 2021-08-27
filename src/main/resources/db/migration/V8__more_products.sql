8b11028ce1441bc24905b4ea1bb029b81799dd90
Knut Nygaard <knutnyg@gmail.com>
2021-08-27 16:31:24 +0200

    More products

---
+++ src/main/resources/db/migration/V8__more_products.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(id, name, price, payload)
+VALUES ('5b0b9c77-a576-411a-bee4-d02770c37689', 'IMAGE_AI_01', 1, null);
+
+INSERT INTO products(id, name, price, payload)
+VALUES ('ccdf8aa2-07d1-46aa-bbf2-c07067c4e117', 'IMAGE_AI_02', 1, null);
+
+INSERT INTO products(id, name, price, payload)
+VALUES ('260d5bbe-faeb-4436-89de-9d8c754a6ab7', 'IMAGE_AI_03', 1, null);
+
+INSERT INTO products(id, name, price, payload)
+VALUES ('ec533145-47fa-464e-8cf0-fd36e3709ad3', 'IMAGE_AI_04', 1, null);
