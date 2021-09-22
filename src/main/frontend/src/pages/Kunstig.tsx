804eb09670ae7c6bcf40c928c4c36f761e084783
Knut Nygaard <knut.nygaard@nav.no>
2021-09-22 18:12:48 +0200

    MVP carousel in image gallery

--- src/main/frontend/src/pages/Kunstig.tsx
+++ src/main/frontend/src/pages/Kunstig.tsx
@@ -9,6 +9,8 @@ import {createOrderInvoice, fetchProduct} from "../product/products";
 import useInterval from "../hooks/useInterval";
 import {updateInvoice} from "../invoice/invoices";
 import {InvoiceView} from "../invoice/Invoice";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 export interface PageProps {
     onChange: (title: string) => void;
@@ -26,9 +28,7 @@ export const Kunstig = (props: PageProps) => {
         access: AccessState.INITIAL
     })
 
-    const [images, setImages] = useState<ImageBlob[]>([
-        {id: 'a1afc48b-23bc-4297-872a-5e7884d6975a', payload: undefined}
-    ])
+    let images
 
     // Hent bundle
     // if bundle:
@@ -44,20 +44,13 @@ export const Kunstig = (props: PageProps) => {
         }
 
         if (state.access === AccessState.ACCESS) {
-            images.forEach((image, index, array) => {
-                if (image.payload === undefined) {
-                    fetchProduct(image.id)
-                        .then(product => {
-                                images[index].payload = product?.payload
-                                setImages(images)
-                            }
-                        )
-                }
-            })
+            images = (<div>
+                <img src={`baseUrl/products/a1afc48b-23bc-4297-872a-5e7884d6975a`}/>
+            </div>)
         }
     })
 
-    const createOrder = () => {
+    const buyAccess = () => {
         createOrderInvoice("ec533145-47fa-464e-8cf0-fd36e3709ad3")
             .then(invoice => setState({...state, invoice: invoice, access: AccessState.PAYMENT_PENDING}))
     }
@@ -81,6 +74,26 @@ export const Kunstig = (props: PageProps) => {
         }
     }, 1000)
 
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: {max: 4000, min: 3000},
+            items: 1
+        },
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: 1
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: 1
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: 1
+        }
+    };
+
     return <div className="page">
         <p>
             These pieces are created by Kunstig, an AI that has taught itself to paint on its own. Kunstig is heavily
@@ -88,25 +101,39 @@ export const Kunstig = (props: PageProps) => {
             can produce an infinite amount of unique artworks, meaning there will never exist two identical pieces.
         </p>
         <h2>Heres a few samples:</h2>
-        <div className="gallery">
-            <img className={"ai-image"} src={pic1}/>
-            <img className={"ai-image"} src={pic2}/>
-            <img className={"ai-image"} src={pic3}/>
-            <img className={"ai-image"} src={pic4}/>
+        <div>
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                showDots={true}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={7000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+            >
+                <img className={"ai-image"} src={pic1}/>
+                <img className={"ai-image"} src={pic2}/>
+                <img className={"ai-image"} src={pic3}/>
+                <img className={"ai-image"} src={pic4}/>
+            </Carousel>
         </div>
 
         <div>
-            <button onClick={createOrder}>Buy article</button>
+            <button onClick={buyAccess}>Purchase a ticket</button>
             {state.access === AccessState.PAYMENT_PENDING && state.invoice &&
             <InvoiceView paymentReq={state.invoice.paymentRequest}/>}
         </div>
 
-        <div className={"awd"}>{
-            images
-                .filter((i => i.payload !== undefined))
-                .map(((image, index) => {
-                    return (<img key={index} src={`data:image/jpeg;base64, ${image.payload}`}/>)
-                }))}</div>
+        {/*<img src={`${baseUrl}/products/a1afc48b-23bc-4297-872a-5e7884d6975a`}/>*/}
+
 
         <p>Next steps:</p>
         <ul>
