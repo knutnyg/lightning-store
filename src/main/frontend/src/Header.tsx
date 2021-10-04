fb2829e451d5da18d6f8a7021fb85d874954c36a
Knut Nygaard <knutnyg@gmail.com>
2021-10-04 20:32:44 +0200

    Better texts header

--- src/main/frontend/src/Header.tsx
+++ src/main/frontend/src/Header.tsx
@@ -7,6 +7,6 @@ export interface HeaderProps {
 
 export const Header = (props: HeaderProps) => <header>
     <h1>️{props.title}</h1>
-    {props.user && <span className="user">Balance: {props.user?.balance}</span>}
-    {!props.user && <span className="user">Not logged in</span>}
+    {props.user && <span className="user">Access</span>}
+    {!props.user && <span className="user"/>}
 </header>
