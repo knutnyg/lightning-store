4f3e879a2144add50eb765800d7a61e11fa288da
Knut Nygaard <knut.nygaard@nav.no>
2021-10-21 20:34:34 +0200

    spacing

--- src/main/frontend/src/pages/Workshop.tsx
+++ src/main/frontend/src/pages/Workshop.tsx
@@ -186,7 +186,7 @@ export const Workshop = (props: Props) => {
                 Velkommen👋 Her kan du be Kunstig (vår AI-modell) male et bilde til galleriet vårt 👩‍🎨
             </p>
             }
-            {state.state === State.INITIAL && <button className="button" onClick={buyImage}>Kjøp</button>}
+            {state.state === State.INITIAL && <button className="button block-xxl" onClick={buyImage}>Kjøp</button>}
             {state.state === State.IN_PAYMENT && <InvoiceView paymentReq={state.imageInvoice?.paymentRequest!!}/>}
             {state.state === State.FETCHING_IMAGE && <div className={"flex-container"}>
                 <p>Kunstig jobber iherdig med å male et bilde til deg.</p>
