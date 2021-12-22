07f29716ddb61c2d93dc70033856df00c87e38ca
Knut Nygaard <knutnyg@gmail.com>
2021-12-22 12:59:51 +0100

    Bump npm dependencies

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,14 +1,8 @@
 import './App.scss';
 import {LSATView} from './pages/Register';
 import {updateUser, useTitle, useUser} from "./hooks/useUser";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route, Link,
-} from "react-router-dom";
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
 import {PaywallView} from "./pages/Blog";
-import {Bitcoin} from "./pages/Bitcoin";
-import {Lightning} from "./pages/Lightning";
 import {Header} from "./Header";
 import {useEffect} from "react";
 import {Admin} from "./pages/Admin";
@@ -58,22 +52,21 @@ function App() {
         <div className="main">
             <Header title={title} user={user}/>
             <div className="content">
-                <Router>
-                    <Switch>
-                        <Route path="/lsat"><LSATView onChange={newtitle}/></Route>
-                        <Route path="/blog-paywall"><PaywallView onChange={newtitle}/></Route>
-                        <Route path="/bitcoin-network"><Bitcoin onChange={newtitle}/></Route>
-                        <Route path="/lightning-network"><Lightning onChange={newtitle}/></Route>
-                        <Route path="/about"><About onChange={newtitle}/></Route>
-                        <Route path="/gallery"><Kunstig onChange={newtitle} updateUser={callbackUpdateUser}
-                                                        user={user}/></Route>
-                        <Route path="/kunstig"><WorkshopWrapper onChange={newtitle} updateUser={callbackUpdateUser}
-                                                                user={user}/></Route>
-                        <Route path="/admin"><Admin onChange={newtitle}/></Route>
-                        <Route path="/"><WorkshopWrapper onChange={newtitle} updateUser={callbackUpdateUser}
-                                                         user={user}/></Route>
-                    </Switch>
-                </Router>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/lsat" element={<LSATView onChange={newtitle}/>}/>
+                        <Route path="/blog-paywall" element={<PaywallView onChange={newtitle}/>}/>
+                        <Route path="/about" element={<About onChange={newtitle}/>}/>
+                        <Route path="/gallery" element={<Kunstig onChange={newtitle} updateUser={callbackUpdateUser}
+                                                                 user={user}/>}/>
+                        <Route path="/kunstig"
+                               element={<WorkshopWrapper onChange={newtitle} updateUser={callbackUpdateUser}
+                                                         user={user}/>}/>
+                        <Route path="/admin" element={<Admin onChange={newtitle}/>}/>
+                        <Route path="/" element={<WorkshopWrapper onChange={newtitle} updateUser={callbackUpdateUser}
+                                                                  user={user}/>}/>
+                    </Routes>
+                </BrowserRouter>
             </div>
         </div>
     );
